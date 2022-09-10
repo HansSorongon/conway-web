319105f1747459228c1ed31f7f5b5b11ae102ea1
HansSorongon <hanssorongon812@gmail.com>
2022-09-10 20:19:59 +0800

    created functions to render drawings

--- components/Canvas.jsx
+++ components/Canvas.jsx
@@ -1,29 +1,78 @@
 import { useRef, useEffect } from 'react'
 import { Box } from '@chakra-ui/react'
 
+import { initialStructure } from './initialStructure.js'
+
 const Canvas = () => {
   const canvasRef = useRef(null)
 
+  const squareHeight = 30
+  const squareWidth = 40
+
+  const drawGrid = (width, height, context) => {
+    for (let i = 1; i <= Math.floor(width / 40); i++) {
+      context.beginPath();
+      context.moveTo(i * squareWidth, 0);
+      context.lineTo(i * squareWidth, height);
+      context.strokeStyle = 'white';
+      context.lineWidth = 1;
+      context.stroke();
+
+      context.beginPath();
+      context.moveTo(0, i * squareHeight);
+      context.lineTo(width, i * squareHeight);
+      context.strokeStyle = 'white';
+      context.lineWidth = 1;
+      context.stroke();
+    }
+  }
+
+  const drawSquare = (x, y, context) => {
+    context.fillStyle = '#7f7f7f'
+    context.fillRect(x * squareWidth, y * squareHeight, squareWidth, squareHeight)
+  }
+
+  let tick = 0
   let i = 0
-  const render = () => {
+  let init = false
+  const render = (initialPosition) => {
     const canvas = canvasRef.current
+    canvas.width = window.innerWidth * 2
+    canvas.height = window.innerHeight * 2
+    canvas.style.width = window.innerWidth
+    canvas.style.height = window.innerHeight
     const context = canvas.getContext('2d')
 
-    context.fillStyle = '#ffffff'
-    context.fillRect(0 + i, 0 , 50, 50)
+    if (tick % 60 == 0) {
+      i++
+    }
 
-    i += 0.1
+    drawGrid(canvas.width, canvas.height, context)
+
+    // initial position
+    // if (init == false) {
+      initialStructure(drawSquare, context)
+      init = true
+    // }
+
+    // all blocks go here
+
+
+
+    tick++
 
     requestAnimationFrame(render)
   }
 
   useEffect(() => {
-    requestAnimationFrame(render)
+    if (!canvasRef.current) return
+    let initialPosition = 'test'
+    requestAnimationFrame(render, initialPosition)
   }, [])
 
   return (
     <Box bg='black' h='calc(150vh)' w='100%' position='absolute' display='flex' justifyContent='center'>
-      <canvas fillStyle="white" ref={canvasRef}/>
+      <canvas fillStyle="white" ref={canvasRef} style={{width: '100%'}}/>
     </Box>
   )
 
---
+++ components/initialStructure.js
@@ -0,0 +1,42 @@
+export const initialStructure = (drawSquare, context) => {
+
+  // pulsar
+  drawSquare(10, 10, context) // top left
+  drawSquare(11, 10, context)
+  drawSquare(12, 10, context)
+
+  drawSquare(13, 9, context)
+  drawSquare(13, 8, context)
+  drawSquare(13, 7, context)
+
+  drawSquare(8, 9, context)
+  drawSquare(8, 8, context)
+  drawSquare(8, 7, context)
+
+  drawSquare(12, 5, context)
+  drawSquare(11, 5, context)
+  drawSquare(10, 5, context)
+
+  drawSquare(10 + 5, 10, context)  // top right
+  drawSquare(11 + 5, 10, context)
+  drawSquare(12 + 5, 10, context)
+
+  drawSquare(13, 9, context)
+  drawSquare(13, 8, context)
+  drawSquare(13, 7, context)
+
+  drawSquare(8, 9, context)
+  drawSquare(8, 8, context)
+  drawSquare(8, 7, context)
+
+  drawSquare(12, 5, context)
+  drawSquare(11, 5, context)
+  drawSquare(10, 5, context)
+
+
+
+
+
+
+}
+
