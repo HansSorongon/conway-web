f8ba786add7d85de0f10874ba0c5e89c811ef77d
HansSorongon <hanssorongon812@gmail.com>
2022-09-11 22:53:23 +0800

    dropped 2d arrays, using objects instead

--- components/Canvas.jsx
+++ components/Canvas.jsx
@@ -14,7 +14,7 @@ const Canvas = () => {
   let tick = 0
   let i = 0
   let init = false
-  let frame = []
+  let frame = [{'x': 10, 'y': 10}, {'x': 11, 'y': 11}, {'x': 12, 'y': 12}]
 
   const render = (arrWidth, arrHeight, initFrame) => { // -------------------------------------- animation loop
 
@@ -60,25 +60,15 @@ const Canvas = () => {
     // }
 
     // initialize
-    if (!init) {
-      frame = initFrame
-      if (frame) {
-        init = true
-      }
-    }
 
     // frame change
-    if (tick % 250 == 0) {
+    if (tick % 60 == 0) {
       // have the arrays change here
       i++
     }
 
-    for (let i = 0; i < arrHeight; i++) {
-      for (let j = 0; j < arrWidth; j++) {
-        if (frame[i][j] == 1) {
-          drawSquare(i, j)
-        }
-      }
+    for (let cell of frame) {
+      drawSquare(cell['x'] + i, cell['y'])
     }
 
     tick++
@@ -92,28 +82,10 @@ const Canvas = () => {
      let arrWidth = Math.floor(canvasRef.current.width / squareWidth) // initializing to 5 and 7 for some reason
      let arrHeight = Math.floor(canvasRef.current.height / squareHeight)
 
-    // create empty 2d-array
-    let initFrame = []
-    for (let i = 0; i < arrHeight; i++) {
-      initFrame.push([])
-      for (let j = 0; j < arrWidth; j++) {
-        initFrame[i].push(0)
-      }
-    }
-
-    console.log(arrHeight)
-    console.log(initFrame)
-
-    initFrame[0][0] = 1
-    initFrame[0][1] = 1
-    initFrame[0][2] = 1
-
-    console.log(initFrame)
-
     // form initial 2d-array here
     //
 
-    requestAnimationFrame(render, arrWidth, arrHeight, initFrame)
+    requestAnimationFrame(render, arrWidth, arrHeight)
 
   }, [])
 
